1cf357f1503f0b4906ab8293abd3ba76ff1d5d26
Liubomyr.Zdrilyi <Liubomyr.Zdrilyi@connectwise.com>
2024-01-24 12:03:08 +0200

    randimize prod classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import { BrowserRouter } from "react-router-dom";
 import { mount } from "marketing/Marketing";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
-console.log(mount);
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
